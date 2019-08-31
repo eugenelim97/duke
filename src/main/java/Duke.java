2f299e853bc2cdb0f3d9d9eb2c4a98fe94259e45
eugenelim997 <e0309924@u.nus.edu>
2019-09-01 02:23:16 +0800

    cleaner code with functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,66 +15,75 @@ public class Duke {
 
         Scanner scanner = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<Task>();
-        //Task[] taskList = new Task[100];
-
-        while (true) {
-            String command = scanner.nextLine();
+        String command = scanner.next();
+        while (!"bye".equals(command)) {
             try {
-                if ("bye".equals(command)) {
-                    System.out.println("Bye. Hope to see you again soon!");
-                    break;
-                } else if ("list".equals(command)) {
-                    int i = 0;
-                    System.out.println("Here are the tasks in your list:");
-                    for (Task Tasks : taskList) {
-                        System.out.println(++i + "." + Tasks.toString());
-                    }
-                } else if (command.startsWith("done")) {
-                    int num = Integer.parseInt(command.replaceAll("[\\D]", ""));
-                    num -= 1;
-                    Task t = taskList.get(num);
-                    t.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("  " + t.toString());
-                } else if (command.startsWith("todo")) {
-                    if (command.equals("todo")) {
-                        throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
-                    }
-                    String description = command.substring(5);
-                    Task t = new Todo(description);
-                    System.out.println("Got it. I've added this task: ");
-                    System.out.println("  " + t.toString());
-                    System.out.println("Now you have " + Task.getTotal() + " tasks in the list.");
-                    taskList.add(t);
-                } else if (command.startsWith("deadline")) {
-                    int index = command.indexOf('/');
-                    index--;
-                    String description = command.substring(9, index);
-                    index += 5;
-                    String by = command.substring(index);
-                    Task t = new Deadline(description, by);
-                    System.out.println("Got it. I've added this task: ");
-                    System.out.println("  " + t.toString());
-                    System.out.println("Now you have " + Task.getTotal() + " tasks in the list.");
-                    taskList.add(t);
-                } else if (command.startsWith("event")) {
-                    int index = command.indexOf('/');
-                    index--;
-                    String description = command.substring(6, index);
-                    index += 5;
-                    String at = command.substring(index);
-                    Task t = new Event(description, at);
-                    System.out.println("Got it. I've added this task: ");
-                    System.out.println("  " + t.toString());
-                    System.out.println("Now you have " + Task.getTotal() + " tasks in the list.");
-                    taskList.add(t);
-                } else {
-                    throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                switch (command) {
+                    case "list":
+                        int i = 0;
+                        System.out.println("Here are the tasks in your list:");
+                        for (Task task : taskList) {
+                            System.out.println(++i + "." + task.toString());
+                        }
+                        break;
+                    case "done":
+                        Task t = taskList.get(scanner.nextInt() - 1);
+                        printDone(t);
+                        break;
+                    case "todo":
+                        String todoInput = scanner.nextLine();
+                        if ("".equals(todoInput)) {
+                            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+                        } else {
+                            Todo todo = new Todo(todoInput);
+                            taskList.add(todo);
+                            printAdd(todo, taskList.size());
+                        }
+                        break;
+                    case "deadline":
+                        String deadlineInput = scanner.nextLine();
+                        if ("".equals(deadlineInput)) {
+                            throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.");
+                        } else {
+                            String[] d = deadlineInput.split("/by", 2);
+                            Deadline deadline = new Deadline(d[0], d[1]);
+                            taskList.add(deadline);
+                            printAdd(deadline, taskList.size());
+                        }
+                        break;
+                    case "event":
+                        String eventInput = scanner.nextLine();
+                        if ("".equals(eventInput)) {
+                            throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.");
+                        } else {
+                            String[] e = eventInput.split("/at", 2);
+                            Event event = new Event(e[0], e[1]);
+                            taskList.add(event);
+                            printAdd(event, taskList.size());
+                        }
+                        break;
+                    default:
+                        scanner.nextLine();
+                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
                 }
-            }
-            catch (DukeException e) {
+                command = scanner.next();
+            } catch (DukeException e) {
                 System.out.println(e.getMessage());
+                command = scanner.next();
             }
         }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printAdd(Task task, int num) {
+        System.out.println("Got it. I've added this task: ");
+        System.out.println("  " + task.toString());
+        System.out.println("Now you have " + num + " tasks in the list.");
+    }
+
+    public static void printDone(Task t) {
+        t.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + t.toString());
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,26 +1,21 @@
 public class Task {
-    private static int totalTasks = 0;
     protected String description;
     protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
-        totalTasks++;
     }
 
     public String getStatusIcon() {
-        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "+" : "-");
     }
 
     public void markAsDone() {
         this.isDone = true;
     }
 
-    public static int getTotal() {
-        return totalTasks;
-    }
     public String toString() {
-        return "{" + this.getStatusIcon() + "} " + description;
+        return "{" + this.getStatusIcon() + "}" + description;
     }
 }
